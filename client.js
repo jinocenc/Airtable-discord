7654fdf3b787e2b7cf9c66d7d00095e825f4f4ee
Jerold Inocencio <jinocenc@Jerolds-MacBook-Pro.local>
2023-01-10 23:45:49 -0800

    ✨ Discord client

--- client.js
+++ client.js
@@ -1,2 +1,9 @@
-import { Client } from 'discord.js';
+import { Client, GatewayIntentBits, Events } from 'discord.js';
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const API_TOKEN = process.env.DISCORD_TOKEN;
+
+client.once(Events.ClientReady, event => {
+	console.log(`Client ready... Logged in as ${event.user.tag}`);
+});
+client.login(API_TOKEN);
