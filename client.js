67f7daddff494055e8439e06fd9b1c2be0d7e121
Jerold Inocencio <jinocenc@Jerolds-MacBook-Pro.local>
2023-01-11 20:14:53 -0800

    🎨 migrated client.js to discord module

--- client.js
+++
@@ -1,9 +0,0 @@
-import { Client, GatewayIntentBits, Events } from 'discord.js';
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const API_TOKEN = process.env.DISCORD_TOKEN;
-
-client.once(Events.ClientReady, event => {
-	console.log(`Client ready... Logged in as ${event.user.tag}`);
-});
-client.login(API_TOKEN);
